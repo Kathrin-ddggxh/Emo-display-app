eb943982aecdfb9b8c37985e5c685ccc3ac4070c
Kathrin-ddggxh <74541092+Kathrin-ddggxh@users.noreply.github.com>
2022-01-18 09:55:37 +0000

    first event listener
    renamed buttons, added functionality to unshiftBtn

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 let emojis = ["🐱‍👓", "🐱‍🐉", "🐱‍💻"];
 
 // Button elements
-const addStartBtn = document.getElementById("add-start-btn");
-const removeStartBtn = document.getElementById("remove-start-btn");
-const addEndBtn = document.getElementById("add-end-btn");
-const removeEndBtn = document.getElementById("remove-end-btn");
+const unshiftBtn = document.getElementById("add-start-btn");
+const shiftBtn = document.getElementById("remove-start-btn");
+const pushBtn = document.getElementById("add-end-btn");
+const popBtn = document.getElementById("remove-end-btn");
 
 const emojiEl = document.getElementById("emoji-container");
-
-console.log(emojis);
-
-for (let i = 0; i < emojis.length; i++) {
-  emojiEl.innerHTML += emojis[i];
-}
+const emojiInput = document.getElementById("emoji-input");
 
 emojiEl.style.fontSize = "80px";
+
+unshiftBtn.addEventListener("click", function () {
+  emojis = [];
+
+  emojis.unshift(emojiInput.value);
+
+  for (let i = 0; i < emojis.length; i++) {
+    emojiEl.innerHTML += emojis[i];
+  }
+
+  emojiInput.value = "";
+});
